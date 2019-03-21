ad77d2f48926042bf9c58d0ec79ea4323f5a437a
dskripka <skripka.dmitry@gmail.com>
2019-03-21 21:52:40 +0300

    change testfile

---
+++ src/testfile.py
@@ -0,0 +1,8 @@
+summ =0
+while True:
+    number = input('Enter the string: \n')
+    if number == 'stop':
+        break
+    if number.isdigit() and int(number) % 5 != 0:
+        summ += int(number)
+print(summ)
